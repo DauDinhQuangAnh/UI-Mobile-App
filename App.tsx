01ea169faff8cc4b7daac06e9c77834c5c039f18
QuangAnh <20521059@gm.uit.edu.vn>
2024-04-23 16:52:24 +0700

    update Page

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import SignIn from '@views/auth/SignIn';
 import LostPassword from '@views/auth/LostPassword';
+import SignIn from '@views/auth/SignIn';
 import Verification from '@views/auth/Verification';
 
 const App = () => {
-  return <Verification />;
+  return <SignIn />;
 };
 
 export default App;
