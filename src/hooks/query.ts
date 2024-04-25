5e713e3d2c5b61958b964e2b35fc938b980f5634
QuangAnh <20521059@gm.uit.edu.vn>
2024-04-26 02:03:55 +0700

    New Commit

--- src/hooks/query.ts
+++ src/hooks/query.ts
@@ -1,11 +1,14 @@
+import {getFromAsyncStorage, Keys} from '@utils/asyncStorage';
 import {useQuery} from 'react-query';
 import {useDispatch} from 'react-redux';
 import {AudioData} from 'src/@types/audio';
 import catchAsyncError from 'src/api/catchError';
-import client from 'src/api/client';
+import {getClient} from 'src/api/client';
 import {upldateNotification} from 'src/store/notification';
+import {Playlist} from 'src/@types/audio';
 
 const fetchLatest = async (): Promise<AudioData[]> => {
+  const client = await getClient();
   const {data} = await client('/audio/latest');
   return data.audios;
 };
@@ -22,6 +25,7 @@ export const useFetchLatestAudios = () => {
 };
 
 const fetchRecommended = async (): Promise<AudioData[]> => {
+  const client = await getClient();
   const {data} = await client('/profile/recommended');
   return data.audios;
 };
@@ -30,6 +34,57 @@ export const useFetchRecommendedAudios = () => {
   const dispatch = useDispatch();
   return useQuery(['recommended'], {
     queryFn: () => fetchRecommended(),
+    onError(err) {
+      const errorMessage = catchAsyncError(err);
+      dispatch(upldateNotification({message: errorMessage, type: 'error'}));
+    },
+  });
+};
+
+const fetchPlaylist = async (): Promise<Playlist[]> => {
+  const client = await getClient();
+  const {data} = await client('/playlist/by-profile');
+  return data.playlist;
+};
+
+export const useFetchPlaylist = () => {
+  const dispatch = useDispatch();
+  return useQuery(['playlist'], {
+    queryFn: () => fetchPlaylist(),
+    onError(err) {
+      const errorMessage = catchAsyncError(err);
+      dispatch(upldateNotification({message: errorMessage, type: 'error'}));
+    },
+  });
+};
+
+const fetchUploadsByProfile = async (): Promise<AudioData[]> => {
+  const client = await getClient();
+  const {data} = await client('/profile/uploads');
+  return data.audios;
+};
+
+export const useFetchUploadsByProfile = () => {
+  const dispatch = useDispatch();
+  return useQuery(['uploads-by-profile'], {
+    queryFn: () => fetchUploadsByProfile(),
+    onError(err) {
+      const errorMessage = catchAsyncError(err);
+      dispatch(upldateNotification({message: errorMessage, type: 'error'}));
+    },
+  });
+};
+
+const fetchFavorites = async (): Promise<AudioData[]> => {
+  const client = await getClient();
+  const {data} = await client('/favorite');
+  return data.audios;
+};
+
+export const useFetchFavorite = () => {
+  const dispatch = useDispatch();
+  return useQuery(['favorite'], {
+    queryFn: () => fetchFavorites(),
     onError(err) {
       const errorMessage = catchAsyncError(err);
       dispatch(upldateNotification({message: errorMessage, type: 'error'}));
