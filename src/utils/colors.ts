495440542fec1840fee24ab8f1d76c6cd865ef78
QuangAnh <20521059@gm.uit.edu.vn>
2024-05-03 22:31:59 +0700

    commit

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,8 +1,8 @@
-const PRIMARY = '#443333';
-const SECONDARY = '#EEA849';
+const PRIMARY = '#274016';
+const SECONDARY = '#ACACAC';
 const CONTRAST = '#FFFFFF';
 const INACTIVE_CONTRAST = 'rgba(255, 255, 255, 0.5)';
-const ERROR = '#FF5B14';
+const ERROR = '#C52828';
 const SUCCESS = '#3BEF1E';
 const OVERLAY = 'rgba(45, 35, 35, 0.5)';
 
